f7eb36a854b0dbf8faf50c22fdfade1b7563cd36
grxu <grxu@uwaterloo.ca>
2022-10-16 00:18:16 -0400

    Update relational model

--- SourceCode/Database/testdb/createtables.sql
+++ SourceCode/Database/testdb/createtables.sql
@@ -11,7 +11,7 @@ CREATE TABLE worldCup
 
 CREATE TABLE matchDetails
    (  
-      match_id            DECIMAL(10, 0) NOT NULL PRIMARY KEY,
+      match_id            DECIMAL(12, 0) NOT NULL PRIMARY KEY,
       stage               VARCHAR(30) NOT NULL,
       home_name           VARCHAR(30) NOT NULL,
       away_name           VARCHAR(30) NOT NULL,
@@ -26,34 +26,21 @@ CREATE TABLE matchDetails
       FOREIGN KEY (year) REFERENCES worldCup 
    );
 
-CREATE TABLE goalDetails
+CREATE TABLE player 
    (
-      goal_time           VARCHAR(30) NOT NULL,
-      player_name         VARCHAR(20) NOT NULL,
-      goal_type           VARCHAR(10) NOT NULL,
-      match_id            DECIMAL(10, 0) NOT NULL,
-      PRIMARY KEY(match_id, goal_time),
-      FOREIGN KEY(match_id) REFERENCES matchDetails
+      player_nationality  VARCHAR(20) NOT NULL,
+      player_name         VARCHAR(15) NOT NULL,
+      PRIMARY KEY (player_nationality, player_name) 
    );
 
-CREATE TABLE team
+CREATE TABLE enrolled
    (
-      team_name           VARCHAR(30) NOT NULL PRIMARY KEY,
-      coach_name          VARCHAR(30)
+      coach_name          VARCHAR(20) NOT NULL,
+      player_nationality  VARCHAR(20) NOT NULL,
+      player_name         VARCHAR(15) NOT NULL,
+      match_id            DECIMAL(12, 0) NOT NULL,
+      PRIMARY KEY (player_name, player_nationality, match_id),
+      FOREIGN KEY (player_name) REFERENCES player,
+      FOREIGN KEY (player_nationality) REFERENCES player,
+      FOREIGN KEY (match_id) REFERENCES matchDetails
    );
-
-CREATE TABLE player
-   (
-      player_name         VARCHAR(30) NOT NULL PRIMARY KEY
-   );
-
-CREATE TABLE enrolledIn 
-   (
-      year                DECIMAL(4, 0) NOT NULL,
-      team_name           VARCHAR(30) NOT NULL,
-      player_name         VARCHAR(30) NOT NULL,
-      PRIMARY KEY(year, team_name, player_name),
-      FOREIGN KEY(team_name) REFERENCES team,
-      FOREIGN KEY(player_name) REFERENCES player,
-   );
-
